f59e1ed67ef2ea56826300d98b3c58b50122e659
Rikuxx0 <rikum0730@gmail.com>
2025-03-20 22:05:19 +0900

    main

--- my_portfolio/src/components/Sidebar.tsx
+++ my_portfolio/src/components/Sidebar.tsx
@@ -4,9 +4,12 @@ import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Drawer from '@mui/material/Drawer';
+import Stack  from '@mui/material/Stack';
 import Typography  from '@mui/material/Typography';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { Rowdies } from 'next/font/google';
 
 
 export default function Sidebar() {
@@ -23,24 +26,56 @@ export default function Sidebar() {
       <Button onClick={toggleDrawer}>
         <ArrowBackIosNewIcon />
       </Button>
-      <Box>
+
+      <Stack direction="row" spacing={3}>
         <Avatar
           alt="Riku Masukawa"
           src='/my_icon.jpg'
-          sx={{ width: 100, height: 100 }}
+          sx={{ width: 120, height: 120 }}
         />
-      </Box>
-      <Box sx={{width: '100%', maxWidth: 500}}>
-        <Typography variant="h4" gutterBottom>
-            Hello Everyone 
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            My name is Riku Masukawa. I'm Security geeker and work on various tools, software and pentration test. Nice to meet you.
-        </Typography>
-        <Typography variant="body1" gutterBottom>
-            趣味でペンテスやWeb開発をしてます。現在は大学生です。
-        </Typography>
-      </Box>
+
+        <Box
+          sx={{
+            width: '100%', // レスポンシブ対応
+            maxWidth: 500, // 最大幅を設定
+            height: 150, // 高さを適宜調整
+            border: 'none',
+            overflow: 'hidden',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+            <iframe
+              src="https://tryhackme.com/api/v2/badges/public-profile?userPublicId=1838215"
+              style={{
+                width: '100%',
+                height: '100%',
+                border: 'none',
+              }}
+            />
+        </Box>
+      </Stack>
+      
+      <Stack direction="column" spacing={1}>
+        <Stack direction="row" spacing={2}>
+          <Button>
+              <GitHubIcon />
+          </Button>
+        </Stack>
+
+        <Box sx={{width: '100%', maxWidth: 500}}>
+          <Typography variant="h4" gutterBottom>
+              Hello Everyone 
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+              My name is Riku Masukawa. I'm Security geeker and work on various tools, software and pentration test. Nice to meet you.
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+              趣味でペンテスやWeb開発をしてます。現在は大学生です。
+          </Typography>
+        </Box>
+      </Stack>
       
 
       
