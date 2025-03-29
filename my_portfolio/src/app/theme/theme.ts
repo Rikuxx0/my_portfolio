8a5e6152e6627f8f386bd940ce65e45454ce478f
Rikuxx0 <rikum0730@gmail.com>
2025-03-29 21:29:47 +0900

    main

--- my_portfolio/src/app/theme/theme.ts
+++ my_portfolio/src/app/theme/theme.ts
@@ -2,6 +2,16 @@ import { createTheme } from "@mui/material";
 
 //カスタムテーマの作成
 const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,    // モバイル
+      sm: 600,  // タブレット
+      md: 960,  // 小型PC
+      lg: 1280, // 大型PC
+      xl: 1920, // 超大型PC
+    },
+  },
+  
   palette: {
       primary : {
           main: "#2A2A2A"
